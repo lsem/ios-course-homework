40ca74bfa8828af26ad398c4fc0ffb0546070149
Lyubomyr Semkiv <lsem@Lyubomyrs-MacBook-Pro.local>
2015-09-16 16:23:04 +0300

    [Added] Initial version of first playground

---
+++ 01-playground/Homework01-play.playground/Contents.swift
@@ -0,0 +1,119 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class DiaryRecord {
+    let creationDate = NSDate()
+    var recordName: String?
+    var text: String?
+    var tags: Set<String>?
+    
+    init() {
+        
+    }
+
+    init (recordName: String, withText text: String, withTags tagsArray: [String]) {
+        self.recordName = recordName
+        self.text = text
+        self.tags = Set<String>()
+        for tag in tagsArray {
+            self.tags?.insert(tag)
+        }
+    }
+    
+    init (text: String, withTags tagsArray: [String]) {
+        self.text = text
+        self.tags = Set<String>()
+        for tag in tagsArray {
+            self.tags?.insert(tag)
+        }
+    }
+
+    init (tagsArray: [String]) {
+        self.tags = Set<String>()
+        for tag in tagsArray {
+            self.tags?.insert(tag)
+        }
+    }
+    
+    
+    init (recordName: String) {
+        self.recordName = recordName
+    }
+    
+    init (recordName: String, withText text: String) {
+        self.recordName = recordName
+        self.text = text
+    }
+    
+    
+    /////////////////////////////////////////////
+    // TODO: Add more descriptive initializers here
+    /////////////////////////////////////////////
+    
+    // TODO: Clarify requirement about date formatting. It probably should be some kind of localized
+    // output but because of my laziness I did not do that.
+    func fullDescription() -> String {
+        var result = ""
+        
+        result += formatCreationDate(self.creationDate)
+        
+        if let recordName = self.recordName {
+            result += "\n" + formatRecordName(recordName)
+        }
+        if let text = self.text {
+            result += "\n" + formatText(text)
+        }
+        if let tags = self.tags {
+            result += "\n" + formatTags(tags)
+        }
+        return result
+    }
+    
+    /// TODO: Use NSDateFormatter class for proper formatting
+    func formatCreationDate(dateValue: NSDate) -> String {
+        return "\(dateValue)"
+    }
+    
+    func formatRecordName(recordNameValue: String) -> String {
+        return recordNameValue
+    }
+    
+    func formatText(textValue: String) -> String {
+        return textValue
+    }
+    
+    func formatTags(tags: Set<String>) -> String {
+        var result = ""
+        if let tags = self.tags {
+            result += "\n"
+            var first = false
+            for tag in tags {
+                if !first {
+                    result += " "
+                } else {
+                    first = false
+                }
+                result += "[\(tag)]"
+            }
+        }
+        return result
+    }
+    
+} // class DiaryRecord
+
+
+// Lets play with this
+
+let record = DiaryRecord()
+let s = record.fullDescription()
+
+
+let recordWithTags = DiaryRecord(recordName: "First", withText: "Started learning Swift. This inconvinient convinience constructors..", withTags: ["жизнь", "боль", "swift", "strange"])
+
+println(recordWithTags.fullDescription())
+
+
+
+
+
